ae1c42e62dc157360a63f7e338faeeda875b1e48
santhipriya28 <santhipriya28@gmail.com>
2023-08-24 17:23:01 -0500

    files created

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -2,14 +2,27 @@ package org.launchcode;
 import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
-        System.out.println("Enter a radius:");
+        Scanner input = new Scanner(System.in);
+
+       /*System.out.println("Enter a radius:");
         Scanner input = new Scanner(System.in);
         double radius = input.nextDouble();
         double A = Circle.getArea(radius);
-        if(!input.hasNext()){
-            System.out.println(" empty ");
-        }
         System.out.println("The area of a circle of radius" + radius + "is :" + A);
+        input.close();*/
+        System.out.println("Enter a radius:");
+        if(input.hasNextDouble()){
+            double radius = input.nextDouble();
 
+            if(radius >0){
+                double A = Circle.getArea(radius);
+                System.out.println("The area of a circle of radius" + radius + "is :" + A);
+            } else {
+                System.out.println("sorry, the radius must be positive number");
+            }
+        } else {
+            System.out.println("sorry that was not a positive number");
+        }
+        input.close();
     }
 }
